8c0da0d62e3b8ece775a9a64c5e49fbfdc1b0eec
Keungeun LEE <keulee42@gmail.com>
2021-08-08 23:16:30 +0200

    functions description added : close read write

--- pipex.c
+++ pipex.c
@@ -4,10 +4,10 @@ int	main(void)
 {
 	int test;
 
-	test = access("test.txt", F_OK);
-	printf("F_OK (not exist): %d\n", test);
-	test = access("here.txt", F_OK);
-	printf("F_OK (exist): %d\n", test);
+	test = unlink("test.txt");
+	printf("unlink test: %d\n", test);
+	// test = access("here.txt", F_OK);
+	// printf("F_OK (exist): %d\n", test);
 
 	return (EXIT_SUCCESS);
 }
