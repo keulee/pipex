b6a0445b23c92e17f96b94e9cc36a5330b4f18dd
Keungeun LEE <keulee42@gmail.com>
2021-08-08 03:54:46 +0200

    note added

--- pipex.c
+++ pipex.c
@@ -2,7 +2,12 @@
 
 int	main(void)
 {
-	printf("pipex base done\n");
+	int test;
+
+	test = access("test.txt", F_OK);
+	printf("F_OK (not exist): %d\n", test);
+	test = access("here.txt", F_OK);
+	printf("F_OK (exist): %d\n", test);
 
 	return (EXIT_SUCCESS);
 }
