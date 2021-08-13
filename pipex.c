7ec30ad5badc356a12796234a28996bd559a621f
Keungeun LEE <keulee42@gmail.com>
2021-08-13 22:04:48 +0200

    code test

--- pipex.c
+++ pipex.c
@@ -5,90 +5,23 @@ int	main(int ac, char **av, char **en)
 	(void)ac;
 	(void)en;
 	(void)av;
-	// int test=20;
-	// pid_t pid;
-
-	// pid = fork();
-	// if (pid == 0)
-	// 	test++;
-	// else
-	// {
-	// 	printf("parents process pid : %d\n", pid);
-	// 	test--;
-	// }
-	
-	// if (pid == -1)
-	// 	printf("child process create failed\n");
-	// else if (pid == 0)
-	// 	printf("child process : %d\n", test);
-	// else
-	// {
-	// 	printf("parents process pid : %d\n", pid);
-	// 	printf("parents process : %d\n", test);
-	// }
-	// int fd;
-	// int fd2;
-
-	// // if (ac == 1)
-	// // 	perror(av[0]);
-
-	// fd = open(av[1], O_RDONLY);
-	// if (fd < 0)
-	// {
-	// 	perror(av[1]);
-	// 	return (-1);
-	// }
-	// dup2(fd, STDIN_FILENO);
-	// execve("/bin/cat", 0,0);
-	// close(fd);
-
-	// fd2 = open(av[2], O_RDWR | O_CREAT, 0644);
-	// if(fd2 < 0)
-	// {
-	// 	perror(av[2]);
-	// 	return (-1);
-	// }
-	// dup2(fd2, STDOUT_FILENO);
-	// close(fd2);
-	// printf("stdout test\n");
-	// int x;
-
-	// x = fork();
-	// if (x == 0)
-	// {
-	// 	printf("child\n");
-	// 	printf("PID: %d\n", getpid());
-	// }
-	// else
-	// {
-
-	// 	printf("parents\n");
-	// 	printf("PID: %d\n", getpid());
-	// 	printf("x(child PID): %d\n", x);
-	// }
-
-	// int i = 1;
-	// if (ac > 1)
-	// {
-	// 	while (av[i])
-	// 	{
-	// 		printf("av : %s\n", av[i]);
-	// 		printf("en : %s\n", en[i]);
-	// 		i++;
-	// 	}
-	// }
-
-	int fd;
-	int fd2;
-
-	fd = open(av[1], O_RDONLY);
-	fd2 = open(av[2], O_RDWR | O_TRUNC);
-	printf("fd: %d\n", fd);
-	printf("here\n");
-
-	dup2(fd2, 1);
-	printf("works or not?\n");
-	// printf("yep\n");
+	int fd[2];
+	int	pid;
+	if (pipe(fd) == -1)
+	{
+		perror("pipe");
+		exit(1);
+	}
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(1);
+	}
+	else if (pid == 0)
+	{
+		close(fd[1]);
+	}
 		
 	return (EXIT_SUCCESS);
 }
